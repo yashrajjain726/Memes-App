2ec595e3ba3072bf23f16420995c83d76efc9aa9
Yashraj Jain <yashraj.jain@bijak.in>
2022-02-13 17:14:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/memes_app/Singleton.kt
@@ -0,0 +1,23 @@
+import android.content.Context
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
+
+class Singleton constructor(context: Context) {
+    companion object {
+        @Volatile
+        private var INSTANCE: Singleton? = null
+        fun getInstance(context: Context) =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Singleton(context).also {
+                    INSTANCE = it
+                }
+            }
+    }
+    private val requestQueue: RequestQueue by lazy {
+        Volley.newRequestQueue(context.applicationContext)
+    }
+    fun <T> addToRequestQueue(req: Request<T>) {
+        requestQueue.add(req)
+    }
+}
